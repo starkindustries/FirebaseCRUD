f384323f9322312271019c865ef3846af5c151d9
Zion Perez <zionperez@gmail.com>
2017-12-30 11:03:28 +0900

    Implement table view controller. Will remove old single-view controller in future commit.

---
+++ FirebaseCRUD/FilesTableViewController.swift
@@ -0,0 +1,67 @@
+//
+//  FilesTableViewController.swift
+//  FirebaseCRUD
+//
+//  Created by Zion Perez on 12/27/17.
+//  Copyright © 2017 Zion Perez. All rights reserved.
+//
+
+import UIKit
+
+class FilesTableViewController: UITableViewController {
+
+    let basicCellIdentifier = "MyBasicCell"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return ""
+        } else {
+            return "Files"
+        }
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: basicCellIdentifier, for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
